831684126f3f4ce1acfe4d28f50cbe01f2e58512
ELCOT <ELCOT@MAHMOOD>
2022-01-31 10:51:49 +0530

    my first commit

---
+++ mit-mahabupkhan/src/firnsbasicproj/HeloWorld.java
@@ -0,0 +1,30 @@
+package firnsbasicproj;
+//good
+public class HeloWorld {
+ public static void main(String[]args) {
+	 System.out.println("Welcome to JAVA...");
+	 int age=20; 
+	 byte bage=20;
+	 short sage=20;
+	 long lage=20;
+	 
+	 float price=20.50f;
+	 double dprice=200.344;
+
+	 char c='z';
+	 char option='y';
+	 char value=97;
+	 
+	 boolean boo=true;
+	 boolean boo2=false;
+	 String name="Firnas is the batch name";
+	 int $nam=20;
+	 int _num=20;
+	 int _nu$m_2;
+	 
+	 
+	 
+	 
+	 
+ }
+}
---
+++ mit-mahabupkhan/src/firnsbasicproj/OperatorsDemo.java
@@ -0,0 +1,19 @@
+package firnsbasicproj;
+//operators demo program
+public class OperatorsDemo {
+public static void main(String[] args) {
+	int num1=20;
+	int num2=3;
+	
+	int mul=num1*num2;
+	System.out.println("Multiplication..:"+mul);
+	int div=num1/num2;
+	System.out.println("Division...:"+div);
+	int add=num1+num2;
+	System.out.println("Addition...:"+add);
+	int sub=num1-num2;
+	System.out.println("Subtraction...:"+sub);
+	
+	
+}
+}
