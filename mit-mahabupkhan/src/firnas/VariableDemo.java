4bc0f4cc627dc47ce0336c8fc9e8b0060d41b30c
ELCOT <ELCOT@MAHMOOD.mshome.net>
2022-02-01 08:53:47 +0530

    variable demo

---
+++ mit-mahabupkhan/src/firnas/VariableDemo.java
@@ -0,0 +1,23 @@
+package firnas;
+
+public class VariableDemo {
+public static void main(String[] args) {
+	byte children=2;
+	short house=1200;
+	int age=47;
+	long distanceToMoon=28320948340823089L;
+	byte b=126;
+	b=127;
+	short s=-32768;
+	float weight=88.45f;
+	weight=1220;
+	double d=34343.56;
+	char initial='m';
+	char c=97;
+	System.out.println("ASCII value of 97 is...:" +c);
+	String aboutMe="I am teacher who teach web 3.0-First in the world...";
+	boolean alive=true;
+			alive=false;
+}
+}
+
